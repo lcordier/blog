add5b2724ac2a0b156334c68bbfdb06ed79cde6c
Louis Cordier <lcordier@gmail.com>
2024-04-12 22:25:25 +0200

    Cosmetic changes.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Blogging is sooo easy, money for nothing and the chicks for free ;)
 
 
-* [20231231](https://lcordier.github.io/blog/20231231)
-* [20240101](https://lcordier.github.io/blog/20240101)
-* [20240102](https://lcordier.github.io/blog/20240102)
-* [20240103](https://lcordier.github.io/blog/20240103)
-* [20240104](https://lcordier.github.io/blog/20240104)
+* [2024-01-04](https://lcordier.github.io/blog/20240104)
+* [2024-01-03](https://lcordier.github.io/blog/20240103)
+* [2024-01-02](https://lcordier.github.io/blog/20240102)
+* [2024-01-01](https://lcordier.github.io/blog/20240101)
+* [2023-12-31](https://lcordier.github.io/blog/20231231)
 
 Clone this blog [here](https://github.com/lcordier/blog).
 
--- build.py
+++ build.py
@@ -3,6 +3,7 @@
 """ Paul Graham's https://www.paulgraham.com/articles.html is like the gold-standard
     for article layouts so I shall follow suit.
 """
+import datetime
 import os
 import re
 
@@ -15,11 +16,12 @@ if __name__ == '__main__':
 
     items = []
 
-    for item in sorted(os.listdir('.')):
+    for item in sorted(os.listdir('.'), reverse=True):
         match = REGEX.match(item)
         if match:
             blog = os.path.splitext(item)[0]
-            items.append(f'* [{blog}](https://lcordier.github.io/blog/{blog})')
+            date = datetime.datetime.strptime(blog, '%Y%m%d').strftime('%Y-%m-%d')
+            items.append(f'* [{date}](https://lcordier.github.io/blog/{blog})')
 
     # Hehe, oldschool...
     open('README.md', 'w').write(header + '\n'.join(items) + footer)
